c4d4f04cf8dd4fea17ab14badd53a45778bfdb1e
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-09-19 14:35:02 +0600

    fixet bugs db psql and added sqlite3

--- currency_exchange/settings.py
+++ currency_exchange/settings.py
@@ -80,16 +80,24 @@ WSGI_APPLICATION = 'currency_exchange.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'currency_exchange',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'bakir03122001',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#     }
+# }
 
 
 # Password validation
