85f36a696bfb85ca889010e58ee2d4fcf852cd41
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-09-20 19:38:38 +0600

    added email conf new

--- account_app/urls.py
+++ account_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from allauth.account.views import ConfirmEmailView
 
 router = DefaultRouter()
 router.register('register', views.UserViewSet)
@@ -9,6 +8,5 @@ router.register('status', views.StatusUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('otp/', views.EmailConfirmationView.as_view())
 ]
